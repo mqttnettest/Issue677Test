cfb59dc43663b41d8f4dba0d1ddfed0d8cb0aaee
SeppPenner <HansM2013@gmx.de>
2019-06-27 09:06:44 +0200

    Initial commit.

---
+++ Issue677Test2/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MQTTnet.AspNetCore;
+using MQTTnet.Server;
+
+namespace Issue677Test2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var mqttOptions = new MqttServerOptionsBuilder()
+                        .WithSubscriptionInterceptor(c => {
+                            // This callback area is not fired regardless tested using breakpoint and Console.WriteLine
+                            Console.WriteLine($"WithSubscriptionInterceptor: client id = {c.ClientId}");
+                            c.AcceptSubscription = false;
+                            c.CloseConnection = true;
+                        })
+                // Other validation + interceptor
+                .Build();
+
+            services.AddHostedMqttServer(mqttOptions);
+            services.AddMqttTcpServerAdapter();
+            services.AddMqttWebSocketServerAdapter();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Issue677Test
+====================================
+
+Issue677Test is a project to test [MQTTnet](https://github.com/chkr1011/MQTTnet) with issue https://github.com/chkr1011/MQTTnet/issues/677.
